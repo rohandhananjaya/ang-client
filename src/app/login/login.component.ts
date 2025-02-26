2934566f07edcb1c691aba51b412254a6c4d8081
Rohan Amarasooriya <RohanA@embla.asia>
2025-02-26 08:57:45 +0530

    Working ng client

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  successMessage: string = '';
+  errorMessage: string = '';
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      const loginData = {
+        username: form.value.username,
+        password: form.value.password
+      };
+
+      this.http.post('https://localhost:7152/login', loginData)
+        .subscribe(
+          (response) => {
+            console.log('Login successful', response);
+            this.successMessage = 'Login successful!';
+            this.errorMessage = '';
+          },
+          (error) => {
+            console.error('Login failed', error);
+            this.errorMessage = 'Login failed. Please check your credentials.';
+            this.successMessage = '';
+          }
+        );
+    }
+  }
+}
